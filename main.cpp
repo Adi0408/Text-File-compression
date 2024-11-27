067a0741f74f1e448c46b2ed153906e0a3552737
Aditya Senapati <126980305+Adi0408@users.noreply.github.com>
2024-11-27 19:24:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,6 +34,39 @@ struct Code {
     vector<int> codeArr;
 };
 
+
+// function for huffman tree , directly using priority queue
+Node* huffmanTree(char arr[], int freq[], int unique_size) {
+    // priority queue - min-heap 
+    priority_queue<Node, vector<Node*>, CompareNode> minHeap;
+
+    // inserting elements into the min heap
+    for (int i = 0; i < unique_size; i++) {
+        minHeap.push(new Node(arr[i], freq[i]));
+    }
+
+    while (minHeap.size() > 1) {
+        // taking first two elements at a time
+        Node* leftNode = minHeap.top();
+        minHeap.pop();
+        Node* rightNode = minHeap.top();
+        minHeap.pop();
+
+        // Create a new internal node
+        Node* parentNode = new Node('$', leftNode->freq + rightNode->freq);
+        parentNode->left = leftNode;
+        parentNode->right = rightNode;
+
+        // Push the parent node back into the priority queue
+        minHeap.push(parentNode);
+    }
+
+    // root of the Huffman Tree
+    Node* root = minHeap.top();
+    return root;
+}
+
+
 // func to check leaf node reached or not
 bool isLeaf(const Node* node) {
     if (node->left == nullptr && node->right == nullptr)
@@ -91,38 +124,6 @@ void huffmandicttofile (ofstream& outputfile, Node* root, vector<int>& codePath)
 
     }
 }
-
-// function for huffman tree , directly using priority queue
-Node* huffmanTree(char arr[], int freq[], int unique_size) {
-    // priority queue - min-heap 
-    priority_queue<Node, vector<Node*>, CompareNode> minHeap;
-
-    // inserting elements into the min heap
-    for (int i = 0; i < unique_size; i++) {
-        minHeap.push(new Node(arr[i], freq[i]));
-    }
-
-    while (minHeap.size() > 1) {
-        // taking first two elements at a time
-        Node* leftNode = minHeap.top();
-        minHeap.pop();
-        Node* rightNode = minHeap.top();
-        minHeap.pop();
-
-        // Create a new internal node
-        Node* parentNode = new Node('$', leftNode->freq + rightNode->freq);
-        parentNode->left = leftNode;
-        parentNode->right = rightNode;
-
-        // Push the parent node back into the priority queue
-        minHeap.push(parentNode);
-    }
-
-    // root of the Huffman Tree
-    Node* root = minHeap.top();
-    return root;
-}
-
 
 
 int main() {
